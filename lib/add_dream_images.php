f464594ff1d752560316efedc93322666569bc46
alyssadisalvo <alyssadisalvo@d-i89-145-61.student.eduroam.uq.edu.au>
2016-09-17 20:15:40 +1000

    EVERYTHING'S WORKING YAYYYYY

--- lib/add_dream_images.php
+++ lib/add_dream_images.php
@@ -7,6 +7,14 @@ if($conn === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 
+$data = json_decode(stripslashes($_POST['data']));
+
+  // here i would like use foreach:
+
+  foreach($data as $d){
+     echo $d;
+  }
+
 //getting the orb id for this orb -- will be the highest since it was just created
 $sql = "SELECT MAX(orbID) FROM Orb";
 $result = mysqli_query($conn, $sql);
@@ -15,8 +23,11 @@ $row = mysqli_fetch_row($result);
 $orbID = $row[0];
 //echo $orbID;
 
-$sql = "UPDATE Orb SET Image_Path_1 = '".$Image_Path_1."' WHERE orbID = '".$orbID."'";
-$sql = "UPDATE Orb SET Image_Path_2 = '".$Image_Path_2."' WHERE orbID = '".$orbID."'";
+$Image_Path_1 = mysqli_real_escape_string($conn, $_POST['Image_Path_1']);
+$Image_Path_2 = mysqli_real_escape_string($conn, $_POST['Image_Path_2']);
+
+
+$sql = "UPDATE Orb SET Image_Path_1 = '".$Image_Path_1."', Image_Path_2 = '".$Image_Path_2."' WHERE orbID = '".$orbID."'";
 
 
 if(mysqli_query($conn, $sql)){
@@ -24,6 +35,7 @@ if(mysqli_query($conn, $sql)){
 } else{
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
 }
+
 
 // close connection
 mysqli_close($conn);
